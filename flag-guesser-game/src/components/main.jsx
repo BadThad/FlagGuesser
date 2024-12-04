ce4f824cbd5a80db03640f3dec112c7cac88f5f7
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-04 15:24:22 +0100

    skapa varaibler med useState som ska användas i function

--- flag-guesser-game/src/components/main.jsx
+++ flag-guesser-game/src/components/main.jsx
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 
+// variables from useState
+const [flag, setFlag] = useState(""); //behåller flaggans url
+const [options, setOptions] = useState([]); //sparar olika länderna
+const [correctCountry, setCorrectCountry] = useState(""); //Sparar rätt svar 
+const [selectedOption, setSelectOption] = useState(null); //det svaret som användaren trycker
+
+
 const FlagGuessGame = () => {
   return (
     <div>
