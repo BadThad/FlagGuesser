ffd063859b44a20ef50d7d6ed06a42c88c47fed1
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-04 12:25:16 +0100

    header in app

--- flag-guesser-game/src/App.jsx
+++ flag-guesser-game/src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
-import './components/footer'
-import './components/header'
-import './components/main'
+import './App.css';
+import './components/footer';
+import './components/header';
+import './components/main';
 
 function App() {
 
   return (
     <>
+    <Header></Header>
     </>
   )
 }
 
-export default App
+export default App;
