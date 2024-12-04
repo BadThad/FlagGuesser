9b89d2b5dca266ce4cca2c53dd5a3834db74536c
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-04 15:52:40 +0100

    random land

--- flag-guesser-game/src/components/main.jsx
+++ flag-guesser-game/src/components/main.jsx
@@ -5,16 +5,19 @@ const FlagGuessGame = () => {
 // variables from useState
 const [flag, setFlag] = useState(""); //behåller flaggans url
 const [options, setOptions] = useState([]); //sparar olika länderna
-const [correctCountry, setCorrectCountry] = useState(""); //Sparar rätt svar 
+let [correctCountry, setCorrectCountry] = useState(""); //Sparar rätt svar 
 const [selectedOption, setSelectOption] = useState(null); //det svaret som användaren trycker
 
 // API Rest countries API
 useEffect(() => { //lagt i för att useEffect ska bestämma när det fetches
+    const apiUrl = "https://restcountries.com/v3.1/all"; //Api som fetchar all data (inga parameter) (ska vara i function annars problem ?)
 const fetchFlag = async () => {
 try {
-    const apiUrl = "https://restcountries.com/v3.1/all"; //Api som fetchar all data (inga parameter) (ska vara i function annars problem)
     const response = await fetch(apiUrl); //request att hämta all data 
     const countries = await response.json(); //ändrar till JSon och är en function
+    const randomIndex = Math.floor(Math.random() * countries.length);
+    let correctCountry = countries[randomIndex]; //att välja random land
+    //length så att det är inom array
  } catch (error) { //error meddelande om de inte gick att hämta api
  } displayError(error.message);
 }
