9e36be3ed955b0275a262277013e9dd2c3090d9e
LEA_HASELSTEINER <leahaselsteiner@hotmail.com>
2024-12-04 12:17:04 +0100

    new components

--- flag-guesser-game/src/App.jsx
+++ flag-guesser-game/src/App.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
 import './App.css'
+import './components/footer'
+import './components/header'
+import './components/main'
 
 function App() {
   const [count, setCount] = useState(0)
